1229c38bfc095f4892e1e162ab068a55dfb357e0
SaurabhThakre04 <142592261+SaurabhThakre04@users.noreply.github.com>
2023-10-15 15:12:42 +0530

    basic todo app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,61 @@
-import logo from './logo.svg';
+import {useState} from 'react'
 import './App.css';
 
 function App() {
+  const [input,setInput] = useState('');
+  const [items,setItems] = useState([])
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    console.log(input);
+    if(input === '')
+    {
+
+    }
+    else{
+     setItems([...items,input])
+      console.log(items);
+     setInput('');
+    }
+  }
+
+  const handleOnClick = (id) =>{
+    const updatedItems = items.filter((elem,ind)=>{
+       return ind !== id;
+    })
+
+    setItems(updatedItems)
+  }
+
+  const handleRemoveAll = ()=>{
+    setItems([]);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+           <div className='heading '><h1>MY TODO LIST </h1></div>
+
+           <div className='todo-form'>
+              <form className='form' onSubmit={handleSubmit}>
+                   <input className='mx-3' type='text' placeholder='what is your task today?' value={input} onChange={(e)=>{setInput(e.target.value)}} ></input>
+                   <button className='btn btn-primary mx-3' type='submit' >ADD</button>
+              </form>
+            </div>
+
+            <div className='your-task'>
+                <h2>MY TASKS</h2>
+                {
+                  items.map((elem,ind)=>{
+                         return (
+                          <div className='tasks d-flex justify-content-between my-2' key={ind}>
+                          <h3 className='mx-2'>{elem}</h3>
+                          <button className='btn btn-secondary' onClick={()=>{handleOnClick(ind)}}>DELETE</button>
+                      </div>
+                    )
+                  })
+                }
+
+                <button className='btn btn-danger' onClick={()=>{handleRemoveAll()}}>REMOVE ALL </button>
+             </div>
     </div>
   );
 }
